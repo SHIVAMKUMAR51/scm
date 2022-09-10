0907935f7e3a141f3816444958c099ec757f77d8
Akshat Tiwari <103210421+alphaviktor@users.noreply.github.com>
2022-09-10 14:24:29 +0530

    Create Bubble.java

---
+++ Bubble.java
@@ -0,0 +1,32 @@
+//this program demostrate the bubbleSorting method
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter the size of your desired array: ";
+    cin>>n;
+
+    int arr[n];
+
+    for (int i=0; i<n; i++){
+        cin>>arr[i];
+    }    
+
+    int counter=1;
+    while(counter<n){
+        for(int i=0; i<n-counter; i++){
+            if(arr[i]>arr[i+1]){
+                int temp=arr[i];
+                arr[i]=arr[i+1];
+                arr[i+1]=temp;
+            }
+        }
+        counter++;
+    }
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
